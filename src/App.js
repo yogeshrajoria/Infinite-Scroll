5d39ef625ca68f2ecc0d505e8acec00b5fb7d9c4
Yogesh Rajoria <yogeshmay30@gmail.com>
2019-11-05 17:03:38 +0530

    [Update]: Cluter removed and git icon fix.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import github from './github.svg';
 import List from './components/List';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <div className="App-brand">Infinite Scroll</div>
         <a href="https://github.com/yogeshrajoria/Infinite-Scroll" target="_blank" rel="noopener noreferrer">
-          <img src={process.env.PUBLIC_URL + '/Github_Logo.png'} className="github" alt="github-link"></img>
+          <img src={github} className="github" alt="github-link"></img>
         </a>
       </header>
         <List />
